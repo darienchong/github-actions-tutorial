5755c79ea19a24f6befeb6d8aecf6bb510cd0b52
Darien Chong Yung Han <e0176647@u.nus.edu>
2021-10-20 19:57:06 +0800

    Remove implementation of PrimeChecker (to be done in branch).

--- src/main/java/org/example/PrimeChecker.java
+++ src/main/java/org/example/PrimeChecker.java
@@ -2,13 +2,7 @@ package org.example;
 
 public class PrimeChecker {
 	public static boolean isPrime(int x) {
-		// Test all divisors up to sqrt(x)
-		int limit = (int) Math.floor(Math.sqrt(x));
-		for (int divisor = 2; divisor <= limit; divisor++) {
-			if (x % divisor == 0) {
-				return false;
-			}
-		}
-		return true;
+		// TODO
+		return false;
 	}
 }
